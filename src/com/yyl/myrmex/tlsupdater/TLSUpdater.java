c6bac4c8b5c84301e27a1a50dca63a5a385dcd66
yyl <yyl.dev@gmail.com>
2012-09-18 09:49:54 -0400

    db added; exporter added

--- src/com/yyl/myrmex/tlsupdater/TLSUpdater.java
+++ src/com/yyl/myrmex/tlsupdater/TLSUpdater.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ClientConnectionManager;
@@ -20,6 +20,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
 
+import com.yyl.myrmex.tlsupdater.database.DatabaseExporter;
+import com.yyl.myrmex.tlsupdater.database.TestDatabaseHelper;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -27,6 +29,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -35,10 +38,9 @@ public class TLSUpdater {
 	private Context context;
 	private PendingIntent alarm;
 	private AlarmManager alarm_manager;
-	private long UPDATE_INTERVAL = 30000;
-	private MyAlarmReceiver my_receiver;
+	private long UPDATE_INTERVAL = 90000;
+	private MyAlarmReceiver my_receiver = null;
 	private IntentFilter intentf;
-	private boolean isReceiverRegistered = false;
 	
 	private int START_DELAY = 2;
 	private String DEBUG_TAG = "TLSUpdater";
@@ -46,16 +48,14 @@ public class TLSUpdater {
 	public TLSUpdater(Context ctx) {
 		context = ctx;
 		alarm_manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		my_receiver = new MyAlarmReceiver();
 		intentf = new IntentFilter("com.yyl.myrmex.tlsupdater.TLSUpdater.uploadalarm");
-		
 	}
 	
 	public void run() {
 		Log.d(DEBUG_TAG, "TLSUpdater.run()");
-		if (isReceiverRegistered == false) {
-			context.registerReceiver(my_receiver, intentf);
-			isReceiverRegistered = true;			
+		if (my_receiver == null) {
+			my_receiver = new MyAlarmReceiver();
+			context.registerReceiver(my_receiver, intentf);			
 		}
 		// get a Calendar object with current time
 		Calendar cal = Calendar.getInstance();
@@ -73,8 +73,9 @@ public class TLSUpdater {
 		Intent intent = new Intent(context, MyAlarmReceiver.class);
 		alarm = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 		alarm_manager.cancel(alarm);
-		if (isReceiverRegistered == true)
+		if (my_receiver != null)
 			context.unregisterReceiver(my_receiver);
+			my_receiver = null;
 	}
 	
 	private class MyAlarmReceiver extends BroadcastReceiver {
@@ -85,7 +86,7 @@ public class TLSUpdater {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			// TODO Auto-generated method stub
-			Log.d(DEBUG_TAG, "Recurring alarm; requesting location tracking.");
+			Log.d(DEBUG_TAG, "Recurring alarm; requesting https connection.");
 			Toast.makeText(context, "hello alarm", Toast.LENGTH_SHORT).show();
 	        // start the service
 	        u = new Updater(context);
@@ -101,29 +102,47 @@ public class TLSUpdater {
 		private HttpResponse getResponse;
 		private Context context;
 		
+		private static final String LOCTABLE_PATH = "/data/com.yyl.myrmex.tlsupdater/databases/test.db";
+		private String DEBUG_TAG = "AsyncTask: Updater";
+		
 		public Updater (Context ctx) {
 			context = ctx;
 		}
 		
 		@Override
 		protected Boolean doInBackground(Void... params) {
-			client = new MyTLSClient(context);
-			httppost = new HttpPost("https://107.22.124.12");
-			try {
-				// Add your data
-		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-		        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-		        nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
-		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-		        // Execute HTTP Post Request
-		        getResponse = client.execute(httppost);
-		        System.out.print(getResponse);
+			TestDatabaseHelper database = new TestDatabaseHelper(context);
+        	DatabaseExporter dbe = new DatabaseExporter(database.getReadableDatabase());
+        	try {
+				dbe.export(Environment.getDataDirectory()+LOCTABLE_PATH);
 				return true;
 			} catch (IOException e) {
 				e.printStackTrace();
 				return false;
 			}
+//			client = new MyTLSClient(context);
+//			httppost = new HttpPost("https://107.22.124.12");
+//			try {
+//				for (int i = 0; i < 5; i++) {
+//					// Add your data
+//			        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//			        nameValuePairs.add(new BasicNameValuePair("id", Integer.toString(i)));
+//			        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//			        // Execute HTTP Post Request
+//			        Log.d(DEBUG_TAG, "send the post packet");
+//			        getResponse = client.execute(httppost);
+//			        Log.d(DEBUG_TAG, "sending complete");
+//			        HttpEntity responseEntity = getResponse.getEntity();
+//			        Log.d(DEBUG_TAG, "get the response");
+//			        System.out.println(responseEntity.getContent());
+//			        Log.d(DEBUG_TAG, "print out the response");
+//				}
+//		        return true;
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				return false;
+//			}
 		}
 		
 		// can use UI thread here
