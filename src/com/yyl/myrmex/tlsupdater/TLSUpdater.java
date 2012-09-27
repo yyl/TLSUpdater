a9db54cd882d4b57c65a1e9d46d470a2bf719de8
yyl <yyl.dev@gmail.com>
2012-09-27 10:08:53 -0400

    file IO util added

--- src/com/yyl/myrmex/tlsupdater/TLSUpdater.java
+++ src/com/yyl/myrmex/tlsupdater/TLSUpdater.java
@@ -1,5 +1,9 @@
 package com.yyl.myrmex.tlsupdater;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -7,6 +11,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.util.Log;
 
 public class TLSUpdater {
@@ -14,10 +19,12 @@ public class TLSUpdater {
 	private Intent alarm_intent;
 	private PendingIntent upload;
 	private AlarmManager alarmm;
-
-	private String dbname;
+	private File logFile, dir;
+	private String dbname = "nodb";
 	private int hour, minute;
+
 	private String DEBUG_TAG = "TLSUpdater";
+	private static final String LOG_PATH = "/tlsupdater";
 
 	public TLSUpdater(Context ctx, String dbname, int hour, int minute) {
 		context = ctx;
@@ -25,6 +32,11 @@ public class TLSUpdater {
 		this.dbname = dbname;
 		this.hour = hour;
 		this.minute = minute;
+		// initialization for file IO, create the folder to contain files
+		dir = new File(Environment.getExternalStorageDirectory(), LOG_PATH);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
 	}
 
 	public void run() {
@@ -33,7 +45,7 @@ public class TLSUpdater {
 		updateTime.setTimeZone(TimeZone.getDefault());
 		updateTime.set(Calendar.HOUR_OF_DAY, this.hour);
 		updateTime.set(Calendar.MINUTE, this.minute);
-//		updateTime.add(Calendar.SECOND, START_DELAY);
+		// updateTime.add(Calendar.SECOND, START_DELAY);
 		Log.i(DEBUG_TAG, "Set the alarm to the time: " + updateTime.getTime());
 
 		alarm_intent = new Intent(context, AlarmReceiver.class);
@@ -42,8 +54,10 @@ public class TLSUpdater {
 		alarm_intent.putExtra("minute", this.minute);
 		upload = PendingIntent.getBroadcast(context, 0, alarm_intent,
 				PendingIntent.FLAG_CANCEL_CURRENT);
-		alarmm.setRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(), AlarmManager.INTERVAL_DAY, upload);
-//		alarmm.setInexactRepeating(AlarmManager.RTC_WAKEUP, updateTime.getTimeInMillis(), UPDATE_INTERVAL, upload);
+		alarmm.setRepeating(AlarmManager.RTC_WAKEUP,
+				updateTime.getTimeInMillis(), AlarmManager.INTERVAL_DAY, upload);
+		// alarmm.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+		// updateTime.getTimeInMillis(), UPDATE_INTERVAL, upload);
 	}
 
 	public void stop() {
@@ -56,6 +70,28 @@ public class TLSUpdater {
 	}
 
 	public void exportSchema(String create_query, String filename) {
-		Log.i(DEBUG_TAG, "Export schema to a file");
+		Log.i(DEBUG_TAG, "Export schema to file " + filename);
+		logFile = new File(dir, filename);
+		if (!logFile.exists()) {
+			try {
+				Log.i(DEBUG_TAG, "File does not exist, creating it now");
+				logFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			// BufferedWriter for performance, true to set append to file flag
+			BufferedWriter buf = new BufferedWriter(new FileWriter(logFile,
+					true));
+			buf.append(create_query);
+			buf.newLine();
+			buf.close();
+			Log.i(DEBUG_TAG, "Writing a new line to the file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
